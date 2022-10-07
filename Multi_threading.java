327acfa9c8be9d57c7beaab4ebb3c4340add6106
ramanujam <rockramanujam2222@gmail.com>
2022-10-07 17:03:43 +0530

    multi basic methods implemented

---
+++ Multi_threading.java
@@ -0,0 +1,40 @@
+package MultiThreading;
+
+public class Multi_threading extends Thread {
+
+	
+	
+	
+	public static void main(String[] args) throws InterruptedException {
+		Thread ob = Thread.currentThread();
+		
+		System.out.println(ob.isInterrupted());
+		ob.start();
+//		ob.setName("Insta");
+//		System.out.println(ob.getName());
+//		System.out.println(ob.getPriority());
+//		ob.setPriority(MAX_PRIORITY);
+//		System.out.println(ob.getPriority());
+		
+//		Multi_threading obj = new Multi_threading();
+//		System.out.println(obj.getName());
+//		obj.setName("suma");
+//		System.out.println(obj.getName());
+//		System.out.println("Im suma");
+//		
+//		Thread ob2 = Thread.currentThread();  
+//        System.out.println(ob2.getName());
+//        
+//		Multi_threading obj1 = new Multi_threading();
+//		System.out.println(obj1.getName());
+//		obj1.setName("suma");
+//		System.out.println(obj1.getName());
+//		System.out.println("Im suma---");
+//	
+//			obj.join(7800);
+//            obj1.join(6000);
+//            Thread ob1 = Thread.currentThread();  
+//            System.out.println(ob1.getName());
+	}
+
+}
